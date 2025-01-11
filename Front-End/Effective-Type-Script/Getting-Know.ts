e72858eb097798f6a49c801d014354487ae7a373
maukan <maukanm22@gmail.com>
2025-01-11 16:56:59 -0500

    added new notes

---
+++ Front-End/Effective-Type-Script/Getting-Know.ts
@@ -0,0 +1,16 @@
+const x = 2 + '3';  // OK
+//    ^? const x: string
+const y = '2' + 3;  // OK
+//    ^? const y: string
+
+const a = null + 7;  // Evaluates to 7 in JS
+//        ~~~~ The value 'null' cannot be used here.
+const b = [] + 12;  // Evaluates to '12' in JS
+//        ~~~~~~~ Operator '+' cannot be applied to types ...
+alert('Hello', 'TypeScript');  // alerts "Hello"
+//             ~~~~~~~~~~~~ Expected 0-1 arguments, but got 2
+
+// The guiding principle of TypeScript’s type system is that it should model JavaScript’s runtime behavior. 
+// But in all of these cases, TypeScript considers it more likely that the odd usage is the result of an error than the developer’s intent, 
+// so it goes beyond simply modeling the runtime behavior. We saw another example of this in the capital/capitol example, where the program didn’t 
+// throw (it logged undefined) but the type checker still flagged an error.
