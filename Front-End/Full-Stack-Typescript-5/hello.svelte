2361681628c42542bbec101891987ea61222d339
maukan <maukanm22@gmail.com>
2025-01-16 20:11:01 -0500

    messing around with svelte

---
+++ Front-End/Full-Stack-Typescript-5/hello.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let name: string = 'World';
+</script>
+<main>
+  <h1>Hello {name}!</h1>
+</main>
+<style>
+  main {
+    text-align: center;
+    color: purple;
+  }
+</style>
